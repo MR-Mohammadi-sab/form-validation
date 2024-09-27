be133471200caefe6386e3ed641625655aa25e95
MR-Mohammadi-sab <asf423796@gmail.com>
2024-09-27 09:20:52 -0700

    add form validation

---
+++ README.md
@@ -0,0 +1,8 @@
+## this is a form validation
+
+#### it makes with HTML CSS JavaScript
+
+deploy Link 👇👇🏿
+
+
+if you wnat to see (https://mr-mohammadi-sab.github.io/joke-taller/)
---
+++ app.js
@@ -0,0 +1,120 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const lastname = document.getElementById("lastname");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validateDefault();
+});
+
+// SetError Method
+function SetError(element, message) {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerHTML = message;
+  inputControl.classList.add("error");
+  inputControl.classList.remove("success");
+}
+// SetSuccess Method
+function SetSuccess(element) {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerHTML = "";
+  inputControl.classList.add("success");
+  inputControl.classList.remove("error");
+}
+
+// Valid email
+function ValidEmail(element) {
+  const lower = element.toLowerCase();
+  const istrue = lower.search("@gmail.com");
+  console.log(istrue);
+  return istrue;
+}
+// ValidateDefault Method
+
+function validateDefault() {
+  const usernameValue = username.value.trim();
+  const lastnameValue = lastname.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
+
+  if (usernameValue === "") {
+    SetError(username, "Username is required");
+  } else {
+    SetSuccess(username);
+  }
+
+  if (lastnameValue === "") {
+    SetError(lastname, "Lastname is required.");
+  } else {
+    SetSuccess(lastname);
+  }
+
+  if (emailValue === "") {
+    SetError(email, "Email is required.");
+  } else if (ValidEmail(emailValue) === -1) {
+    SetError(email, "Provide a valid email address.");
+  } else {
+    SetSuccess(email);
+  }
+
+  if (passwordValue === "") {
+    SetError(password, "Password is required.");
+  } else if (passwordValue.length < 8) {
+    SetError(password, "Password must be at least 8 character.");
+  } else {
+    SetSuccess(password);
+  }
+
+  if (password2Value === "") {
+    SetError(password2, "Please confirm your password.");
+  } else if (password2Value !== passwordValue) {
+    SetError(password2, "Passwords doesn't match.");
+  } else {
+    SetSuccess(password2);
+  }
+}
+
+// // function seterror
+// function SetError(element, message) {
+//   const inputControl = element.parentElement;
+//   const errorDisplay = inputControl.querySelector(".error");
+
+//   errorDisplay.innerHTML = message;
+//   inputControl.classList.add("error");
+//   inputControl.classList.remove("seccuss");
+// }
+
+// // function SetSeccuss
+
+// function SetSeccuss(element) {
+//   const inputControl = element.parentElement;
+//   const errorDisplay = inputControl.querySelector(".error");
+
+//   errorDisplay.innerHTML = "";
+//   inputControl.classList.add("success");
+//   inputControl.classList.remove("error");
+// }
+
+// //  Validate Method
+
+// function validateDefault() {
+//   const usernameValue = username.value;
+//   const lastnameValue = lastname.value;
+//   const emailValue = email.value;
+//   const passwordValue = password.value.trim();
+//   const password2Value = password2.value.trim();
+
+//   if (usernameValue === "") {
+//     SetError(username, "The username is required.");
+//   } else {
+//     SetSeccuss(username);
+//   }
+// }
